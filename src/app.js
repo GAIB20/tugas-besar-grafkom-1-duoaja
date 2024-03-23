abefd532ec8dab0e36d92648fa745f1dca979f0d
henry anand <13521004@std.stei.itb.ac.id>
2024-03-23 20:40:28 +0700

    refactor : webgl setup

--- src/app.js
+++ src/app.js
@@ -1,63 +1,21 @@
-import { compileShader } from "./utils/WebGLUtils.js";
 import { Line } from "./models/line.js";
 import { Square } from "./models/square.js";
 import { Rectangle } from "./models/rectangle.js";
+import { initializeWebGL } from "./utils/WebGLSetup.js";
 
 // Get the canvas element
 const canvas = document.getElementById("canvas");
 
-const gl = canvas.getContext("webgl");
-if (!gl) {
-  alert("Unable to initialize WebGL. Your browser may not support it.");
-}
-
-const vertexShaderSource = `
-      attribute vec2 a_position;
-      uniform vec2 u_resolution;
-      
-      void main() {
-          // Convert position from pixels to 0.0 to 1.0
-          vec2 zeroToOne = a_position / u_resolution;
-          
-          // Convert from 0->1 to 0->2
-          vec2 zeroToTwo = zeroToOne * 2.0;
-          
-          // Convert from 0->2 to -1->+1 (clip space)
-          vec2 clipSpace = zeroToTwo - 1.0;
-          
-          gl_Position = vec4(clipSpace, 0, 1);
-      }
-  `;
-
-const fragmentShaderSource = `
-      precision mediump float;
-      void main() {
-          gl_FragColor = vec4(1, 0, 0, 1);
-      }
-  `;
-
-const vertexShader = compileShader(gl, vertexShaderSource, gl.VERTEX_SHADER);
-const fragmentShader = compileShader(
+// Initialize WebGL
+const {
   gl,
-  fragmentShaderSource,
-  gl.FRAGMENT_SHADER
-);
-
-const program = gl.createProgram();
-gl.attachShader(program, vertexShader);
-gl.attachShader(program, fragmentShader);
-gl.linkProgram(program);
-gl.useProgram(program);
-
-const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-const resolutionUniformLocation = gl.getUniformLocation(
   program,
-  "u_resolution"
-);
+  positionAttributeLocation,
+  resolutionUniformLocation,
+  positionBuffer,
+} = initializeWebGL(canvas);
 
-const positionBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
+// TODO : Refactor Model
 const drawLineButton = document.getElementById("draw-line");
 const line = new Line(
   gl,
@@ -120,6 +78,10 @@ drawRectangleButton.addEventListener("click", function () {
   rectangle.toggleDrawMode();
 });
 
+const saveButton = document.getElementById("save");
+saveButton.addEventListener("click", function () {
+  // line.save();
+});
 // clear
 const clearButton = document.getElementById("clear");
 clearButton.addEventListener("click", function () {
---
+++ src/utils/WebGLSetup.js
@@ -0,0 +1,65 @@
+import { compileShader } from "./WebGLUtils.js";
+
+export function initializeWebGL(canvas) {
+  const gl = canvas.getContext("webgl");
+  if (!gl) {
+    console.error(
+      "Unable to initialize WebGL. Your browser may not support it."
+    );
+    return null;
+  }
+
+  const vertexShaderSource = `
+    attribute vec2 a_position;
+    uniform vec2 u_resolution;
+
+    void main() {
+        // Convert position from pixels to 0.0 to 1.0
+        vec2 zeroToOne = a_position / u_resolution;
+        
+        // Convert from 0->1 to 0->2
+        vec2 zeroToTwo = zeroToOne * 2.0;
+        
+        // Convert from 0->2 to -1->+1 (clip space)
+        vec2 clipSpace = zeroToTwo - 1.0;
+        
+        gl_Position = vec4(clipSpace, 0, 1);
+    }
+    `;
+
+  const fragmentShaderSource = `
+        precision mediump float;
+        void main() {
+            gl_FragColor = vec4(1, 0, 0, 1);
+        }
+        `;
+
+  const vertexShader = compileShader(gl, vertexShaderSource, gl.VERTEX_SHADER);
+  const fragmentShader = compileShader(
+    gl,
+    fragmentShaderSource,
+    gl.FRAGMENT_SHADER
+  );
+
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  gl.useProgram(program);
+
+  const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  const resolutionUniformLocation = gl.getUniformLocation(
+    program,
+    "u_resolution"
+  );
+
+  const positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  return {
+    gl,
+    program,
+    positionAttributeLocation,
+    resolutionUniformLocation,
+    positionBuffer,
+  };
+}
