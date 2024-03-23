1f7cfedff5a4250d3f7178a3e45fa99e580ad67b
henry anand <13521004@std.stei.itb.ac.id>
2024-03-23 17:57:13 +0700

    refactor : line model with mouse gesture

--- src/models/line.js
+++ src/models/line.js
@@ -1,27 +1,76 @@
 export class Line {
-    constructor(gl, program, positionAttributeLocation, resolutionUniformLocation, canvas) {
-        this.gl = gl;
-        this.program = program;
-        this.positionAttributeLocation = positionAttributeLocation;
-        this.resolutionUniformLocation = resolutionUniformLocation;
-        this.canvas = canvas;
-        this.positions = new Float32Array(4);
+  constructor(
+    gl,
+    program,
+    positionAttributeLocation,
+    resolutionUniformLocation,
+    canvas
+  ) {
+    this.gl = gl;
+    this.program = program;
+    this.positionAttributeLocation = positionAttributeLocation;
+    this.resolutionUniformLocation = resolutionUniformLocation;
+    this.canvas = canvas;
+    this.positions = new Float32Array(4);
+    this.drawLineMode = false;
+    this.isPressed = false;
+
+    this.mouseDownHandler = this.mouseDownHandler.bind(this);
+    this.mouseMoveHandler = this.mouseMoveHandler.bind(this);
+    this.mouseUpHandler = this.mouseUpHandler.bind(this);
+  }
+
+  mouseDownHandler(e) {
+    this.isPressed = true;
+    this.x1 = e.clientX - this.canvas.offsetLeft;
+    this.y1 = this.canvas.clientHeight - e.clientY + this.canvas.offsetTop;
+  }
+
+  mouseUpHandler() {
+    this.isPressed = false;
+  }
+
+  mouseMoveHandler(e) {
+    if (this.isPressed && this.drawLineMode) {
+      this.x2 = e.clientX - this.canvas.offsetLeft;
+      this.y2 = this.canvas.clientHeight - e.clientY + this.canvas.offsetTop;
+      this.updateCoordinates(this.x1, this.y1, this.x2, this.y2);
+      this.draw();
     }
+  }
+  toggleDrawLineMode() {
+    this.drawLineMode = !this.drawLineMode;
+  }
+  activate() {
+    this.canvas.addEventListener("mousedown", this.mouseDownHandler);
+    this.canvas.addEventListener("mouseup", this.mouseUpHandler);
+    this.canvas.addEventListener("mousemove", this.mouseMoveHandler);
+  }
+  updateCoordinates(x1, y1, x2, y2) {
+    this.positions = new Float32Array([x1, y1, x2, y2]);
+  }
 
-    updateCoordinates(x1, y1, x2, y2) {
-        this.positions = new Float32Array([x1, y1, x2, y2]);
-    }
+  draw() {
+    const gl = this.gl;
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ARRAY_BUFFER, this.positions, gl.STATIC_DRAW);
 
-    draw() {
-        const gl = this.gl;
-        gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-        gl.bufferData(gl.ARRAY_BUFFER, this.positions, gl.STATIC_DRAW);
+    gl.uniform2f(
+      this.resolutionUniformLocation,
+      this.canvas.width,
+      this.canvas.height
+    );
+    gl.enableVertexAttribArray(this.positionAttributeLocation);
+    gl.vertexAttribPointer(
+      this.positionAttributeLocation,
+      2,
+      gl.FLOAT,
+      false,
+      0,
+      0
+    );
 
-        gl.uniform2f(this.resolutionUniformLocation, this.canvas.width, this.canvas.height);
-        gl.enableVertexAttribArray(this.positionAttributeLocation);
-        gl.vertexAttribPointer(this.positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
-
-        gl.clear(gl.COLOR_BUFFER_BIT);
-        gl.drawArrays(gl.LINES, 0, 2);
-    }
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.LINES, 0, 2);
+  }
 }
